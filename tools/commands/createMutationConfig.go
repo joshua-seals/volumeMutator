737ce6c8ccc02db52add592f3d52af99200df2f6
Joshua Seals <jseals@renci.org>
2023-12-05 06:50:46 -0500

    Updates ot mutationconfig file

--- tools/commands/createMutationConfig.go
+++ tools/commands/createMutationConfig.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"os"
 
-	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
 	v1 "k8s.io/api/admissionregistration/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -25,30 +24,33 @@ func CreateMutationConfig(ctx context.Context, caCertPath string) {
 	}
 
 	path := "/mutate"
-	fail := admissionregistrationv1.Fail
+	fail := v1.Fail
 	// Read in caCert created in GenerateTLSCerts
 	caCert, err := os.ReadFile(caCertPath)
+	if err != nil {
+		panic("failed to read certPath")
+	}
 
-	mutateconfig := &admissionregistrationv1.MutatingWebhookConfiguration{
+	mutateconfig := &v1.MutatingWebhookConfiguration{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: mutationCfgName,
 		},
-		Webhooks: []admissionregistrationv1.MutatingWebhook{{
+		Webhooks: []v1.MutatingWebhook{{
 			Name: "volume-mutator.renci.org",
-			ClientConfig: admissionregistrationv1.WebhookClientConfig{
+			ClientConfig: v1.WebhookClientConfig{
 				CABundle: caCert, // CA bundle created in generateTLSCerts command
-				Service: &admissionregistrationv1.ServiceReference{
+				Service: &v1.ServiceReference{
 					Name:      webhookService,
 					Namespace: webhookNamespace,
 					Path:      &path,
 				},
 			},
-			Rules: []admissionregistrationv1.RuleWithOperations{
+			Rules: []v1.RuleWithOperations{
 				{
-					Operations: []admissionregistrationv1.OperationType{
-						admissionregistrationv1.Create, admissionregistrationv1.Update,
+					Operations: []v1.OperationType{
+						v1.Create, v1.Update,
 					},
-					Rule: admissionregistrationv1.Rule{
+					Rule: v1.Rule{
 						APIGroups:   []string{"apps"},
 						APIVersions: []string{"v1"},
 						Resources:   []string{"deployments"},
